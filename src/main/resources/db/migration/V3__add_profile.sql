88319b45951c75619a73f9ca1595d1a0659b8526
Bhanuka-botheju <wpbhanukabotheju@gmail.com>
2025-08-20 12:03:10 +0530

    add to V3 & V4 migration

---
+++ src/main/resources/db/migration/V3__add_profile.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS profile (
+    id BIGINT PRIMARY KEY,
+    bio TEXT,
+    phone_number VARCHAR(15),
+    date_of_birth DATE,
+    loyalty_points INT DEFAULT 0 CHECK (loyalty_points >= 0),
+    FOREIGN KEY (id) REFERENCES users(id)
+);
